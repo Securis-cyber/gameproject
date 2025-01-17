683accbeedd3ed0e30e9955ad3ce930ce41fa28f
Securis-cyber <lobrien1992@gmail.com>
2025-01-17 17:13:14 +1000

    Initialise repo, add starter code

---
+++ project/axe_game.cpp
@@ -0,0 +1,44 @@
+#include "raylib.h"
+
+
+
+int main()
+{
+    int width{1350};
+    int height{1200};
+    InitWindow(width, height, "WindowName");
+
+
+    // circle coordinates
+    int circle_x{675};
+    int circle_y{600};
+    
+
+    SetTargetFPS(60);
+    while (WindowShouldClose() == false){
+        BeginDrawing();
+        ClearBackground(BLUE);
+
+        // game logic begins
+        DrawCircle(circle_x, circle_y, 25.0, RED);
+        if (IsKeyDown(KEY_D) && circle_x < width)
+        {
+            circle_x += 10;
+        }
+        if (IsKeyDown(KEY_A) && circle_x > 0)
+        {
+            circle_x -= 10;
+        }
+        if (IsKeyDown(KEY_S) && circle_y < height)
+        {
+            circle_y += 10;
+        }
+        if (IsKeyDown(KEY_W) && circle_y > 0)
+        {
+            circle_y -= 10;
+        }
+        // game logic ends
+        EndDrawing();
+    }
+
+} 
---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+string a = "";
+
+int main()
+{
+    cout <<" Enter your name "; 
+    cin >> a;
+    cout << "HELLO " << a;
+    printf("Hello");
+}
