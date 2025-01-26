eaaf03b6c4c07ba53fe4626f0a5d16603a7706f0
Securis-cyber <lobrien1992@gmail.com>
2025-01-27 08:59:25 +1000

    Refactor scale to const, add undo movement to loop

--- classy clash/top-down-section/classy_clash.cpp
+++ classy clash/top-down-section/classy_clash.cpp
@@ -6,10 +6,12 @@ int main()
 {
 
     const int window_dimensions[2]{384, 384};
+    const float map_scale{4.0};
     InitWindow(window_dimensions[0], window_dimensions[1], "Classy Clash");
 
     Texture2D map = LoadTexture("nature_tileset/worldmap.png");
     Vector2 map_position{0.0, 0.0};
+    
 
     Character knight;
     knight.set_screen_position(window_dimensions[0],window_dimensions[1]);
@@ -23,8 +25,16 @@ int main()
         BeginDrawing();
         map_position = Vector2Scale(knight.get_world_position(), -1.f);
         ClearBackground(WHITE);
-        DrawTextureEx(map, map_position, 0.0, 4.0, WHITE);
+        DrawTextureEx(map, map_position, 0.0, map_scale, WHITE);
         knight.tick(GetFrameTime());
+        // check map boundaries
+        if (knight.get_world_position().x < 0.f ||
+            knight.get_world_position().y < 0.f ||
+            knight.get_world_position().x + window_dimensions[0] > map.width * map_scale ||
+            knight.get_world_position().y + window_dimensions[1] > map.height * map_scale)
+            {
+                knight.undo_movement();
+            }
 
         EndDrawing();
     }
